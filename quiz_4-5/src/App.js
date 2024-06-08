376865ba905112396d67561544323dd0e31faf08
dmacST <58571753+dmacST@users.noreply.github.com>
2024-06-08 19:13:20 +0400

    late

--- quiz_4-5/src/App.js
+++ quiz_4-5/src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
+import Home from './pages/Home'; // Import the Home component
 import './App.css';
-import iphoneImage from './img/iphone.png'; // Make sure to add the image to the public or src folder
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <h1 className="text-title">iPhone 12 and iPhone 12 Mini</h1>
-        <h2 className="text-main">Blast Past Fast.</h2>
-        <p className="text-desc">
-          A superpowerful chip. 5G speed. An advanced /n
-          dual-camera system. A Ceramic Shield front that's tougher than any smartphone glass. And a bright, beautiful OLED display. iPhone 12 has it all - in two great sizes.
-        </p>
-        <p className="text-price">From $24.95/mo. for 24mo. or $599 before trade-in*</p>
-        <button className="buy-button">Buy</button>
-        <div className="image-container">
-          <img src={iphoneImage} alt="iPhone" className="iphone-image" />
-        </div>
-      </div>
+    <div>
+      <Home /> {/* Render the Home component */}
     </div>
   );
 }
--- quiz_4-5/src/pages/Home.js
+++ quiz_4-5/src/pages/Home.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import './App.css';
-import iphoneImage from './iphone12.jpg'; // Make sure to add the image to the public or src folder
+import '../App.css';
+import iphoneImage from '../img/iphone.png'; // Make sure to add the image to the public or src folder
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="medium-text">iPhone 12 and iPhone 12 Mini</h1>
-        <h2 className="big-text">Blast Past Fast.</h2>
-        <p className="small-text">
-          A superpowerful chip. 5G speed. An advanced dual-camera system. A Ceramic Shield front that's tougher than any smartphone glass. And a bright, beautiful OLED display. iPhone 12 has it all - in two great sizes.
+        <h1 className="text-title">iPhone 12 and iPhone 12 Mini</h1>
+        <h2 className="text-main">Blast past fast.</h2>
+        <p className="text-desc">
+          A superpowerful chip. 5G speed. An advanced <br />
+          dual-camera system. A Ceramic Shield front that's tougher than <br />
+          any smartphone glass. And a bright, beautiful OLED display. <br />
+          iPhone 12 has it all - in two great sizes.
         </p>
-        <img src={iphoneImage} alt="iPhone 12" className="iphone-image"/>
+        <p className="text-price">From $24.95/mo. for 24mo. or $599 before trade-in*</p>
+        <button className="buy-button">Buy</button>
+        <div className="image-container">
+          <img src={iphoneImage} alt="iPhone" className="iphone-image" />
+        </div>
       </div>
     </div>
   );
